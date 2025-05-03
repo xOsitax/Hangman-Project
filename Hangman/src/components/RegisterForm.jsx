a0161179ecfad970f1d89b32cf5d96ccbecfd557
Yiting Zhang <zhangyiting.yiting@outlook.com>
2025-05-03 12:30:15 +0200

    filled in refisterform.jsx

---
+++ Hangman/src/components/RegisterForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function RegisterForm(){
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleRegister() {
+
+    if (!name || !password) {
+      alert("Please fill in both fields.");
+      return;
+    }
+    
+    const storedAccounts = JSON.parse(localStorage.getItem('accounts')) || [];
+
+    if (storedAccounts.some((account) => account.name === name)) {
+      alert("This account already exists.");
+      return;
+    }
+
+    const newAccount = { name, password };
+    storedAccounts.push(newAccount); 
+
+
+    localStorage.setItem('accounts', JSON.stringify(storedAccounts));
+    alert("Registration successful!");
+    
+    setName('');
+    setPassword('');
+  }
+
+return (
+  <div>
+  <h1>Register</h1>
+  <input 
+  type="text"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+  placeholder="Name"
+  />
+
+<input 
+  type="password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  placeholder="Password"
+  />
+  
+  <button onClick={handleRegister}>Register</button>
+  </div>
+ );
+}
+
+
+
+export default RegisterForm;
