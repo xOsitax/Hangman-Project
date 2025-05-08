721bf044342e2744bc515d5605b8be1bb0f1cc83
Lars <lars@recap.dk>
2025-05-08 14:30:04 +0200

    updated max length word to 8 char

--- Hangman/src/components/Api.jsx
+++ Hangman/src/components/Api.jsx
@@ -4,11 +4,23 @@ function RandomWord() {
   const [word, setWord] = useState("");
   const [guess, setGuess] = useState("");
 
-  useEffect(() => {
+  // this part fetches word and discards ones over 8 characters
+  const fetchWord = () => {
     fetch("https://random-word-api.herokuapp.com/word?number=1")
       .then((response) => response.json())
-      .then((data) => setWord(data[0]))
+      .then((data) => {
+        const fetchedWord = data[0];
+        if (fetchedWord.length <= 8) {
+          setWord(fetchedWord);
+        } else {
+          fetchWord();
+        }
+      })
       .catch((error) => console.error("Error fetching word:", error));
+  };
+
+  useEffect(() => {
+    fetchWord();
   }, []);
 
   return (
