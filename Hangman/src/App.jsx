c397e5104895864d9d0b593a7d4c36d2a82dc045
VimalaSellappan <Vimala.Sellappan@stud.sundsgarden.se>
2025-05-11 05:53:27 +0200

    home page UI completed with responsive design and animations

--- Hangman/src/App.jsx
+++ Hangman/src/App.jsx
@@ -1,34 +1,22 @@
-<<<<<<< HEAD
-import HangmanGameLogic from './components/HangmanGameLogic';
-import './index.css'
-
-function App() {
-  return (
-    <div className="App">
-      <HangmanGameLogic />
-    </div>
-  );
-}
-
-export default App;
-=======
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RegisterForm from './components/RegisterForm';
-import LoginForm from './components/LoginForm'; 
+import LoginForm from './components/LoginForm';
+import HangmanGameLogic from './components/HangmanGameLogic';
+import './index.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/game" element={<HangmanGameLogic />} /> 
       </Routes>
     </Router>
   );
 }
 
 export default App;
->>>>>>> origin/Authentication-System
